4e5ecaf3b28d099c704be64484ed47ed81aa415f
josueibecerra <47013134+josueibecerra@users.noreply.github.com>
2019-07-30 23:40:54 -0400

    game finished. could use improvements

--- Bounce!.py
+++ Bounce!.py
@@ -12,8 +12,9 @@ root.update()
 
 
 class Ball:
-    def __init__(self, canvas, color):
+    def __init__(self, canvas, paddle, color):
         self.canvas = canvas
+        self.paddle = paddle
         self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
         self.canvas.move(self.id, 245, 100)
         start = [-3, -2, -1, 0, 1, 2, 3]
@@ -22,6 +23,14 @@ class Ball:
         self.y = -3
         self.canvas_height = self.canvas.winfo_height()
         self.canvas_width = self.canvas.winfo_width()
+        self.hit_bottom = False
+
+    def hit_paddle(self, position):
+        paddle_position = self.canvas.coords(self.paddle.id)
+        if position[2] >= paddle_position[0] and position[0] <= paddle_position[2]:
+            if position[3] >= paddle_position[1] and paddle_position[3] <= paddle_position[3]:
+                return True
+            return False
 
     def draw(self):
         self.canvas.move(self.id, self.x, self.y)
@@ -30,17 +39,48 @@ class Ball:
         if position[1] <= 0:
             self.y = 3
         if position[3] >= self.canvas_height:
-            self.y = -3
+            self.hit_bottom = True
+            canvas.create_text(245, 100, text='Game Over')
         if position[0] <= 0:
             self.x = 3
         if position[2] >= self.canvas_width:
             self.x = -3
+        if self.hit_paddle(position) == True:
+            self.y = -3
 
 
-ball = Ball(canvas, 'red')
+class Paddle:
+    def __init__(self, canvas, color):
+        self.canvas = canvas
+        self.id = canvas.create_rectangle(0, 0, 100, 10, fill=color)
+        self.canvas.move(self.id, 200, 300)
+        self.x = 0
+        self.canvas_width = self.canvas.winfo_width()
+        self.canvas.bind_all('<KeyPress-Left>', self.turn_left)
+        self.canvas.bind_all('<KeyPress-Right>', self.turn_right)
+
+    def draw(self):
+        self.canvas.move(self.id, self.x, 0)
+        position = self.canvas.coords(self.id)
+        if position[0] <= 0:
+            self.x = 0
+        if position[2] >= self.canvas_width:
+            self.x = 0
+
+    def turn_left(self, event):
+        self.x = -2
+
+    def turn_right(self, event):
+        self.x = 2
+
+
+paddle = Paddle(canvas, 'blue')
+ball = Ball(canvas, paddle, 'red')
 
 while True:
-    ball.draw()
+    if ball.hit_bottom == False:
+        ball.draw()
+        paddle.draw()
     root.update_idletasks()
     root.update()
-    time.sleep(0.01)
+    time.sleep(0.07)
